f639d55189b30130c1ad375da61ba8c430935196
bipinanima@gmail.com <bipinanima@gmail.com>
2019-05-12 03:33:50 +0530

    latest chnages

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
+
+//path Location
+
+ app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+ })
+
+ console.log("this is working");
